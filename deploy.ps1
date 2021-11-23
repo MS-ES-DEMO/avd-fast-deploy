9364a357749e8849da17324256fdff2d87a3837f
mlrcloud <manuellazaror@gmail.com>
2021-11-23 18:09:00 +0100

    personalDesktopAssignmentType

--- deploy.ps1
+++ deploy.ps1
@@ -7,9 +7,9 @@ Import-Module Tools
 $deploymentName="AVD-Deployment-$(New-Guid)"
 
 $localVmAdminPassword = randomPassword
-#$existingDomainAdminPassword = 'XgXqIlT6LYBydGnhvKd\cue/9q5k'#TODO: ONLY FOR TESTING PURPOSES
+$existingDomainAdminPassword = 'XgXqIlT6LYBydGnhvKd\cue/9q5k'#TODO: ONLY FOR TESTING PURPOSES
 
-$existingDomainAdminPassword = 'S1stemcenteravd2021'
+#$existingDomainAdminPassword = 'S1stemcenteravd2021'
 
 $params = "{ \`"localVmAdminPassword\`":{\`"value\`": \`"${localVmAdminPassword}\`" }, \`"existingDomainAdminPassword\`":{\`"value\`": \`"${existingDomainAdminPassword}\`" } }"
 
@@ -17,4 +17,4 @@ $params = "{ \`"localVmAdminPassword\`":{\`"value\`": \`"${localVmAdminPassword}
 # TODO: Ensure the parameters.json file is up to date
 # TODO: For production deployments, update the deployment parameter file in the command below.
 
-az deployment sub create -l westeurope -n $deploymentName --template-file '.\avd\main.bicep' --parameters '.\personal.parameters.virtualdom.json' --parameters $params
+az deployment sub create -l westeurope -n $deploymentName --template-file '.\avd\main.bicep' --parameters '.\personal.parameters.json' --parameters $params
