99513d7e986176b09fab608100c31bd53d2ee0b5
mlrcloud <manuellazaror@gmail.com>
2021-11-25 16:55:05 +0100

    testing apps

--- deploy.ps1
+++ deploy.ps1
@@ -6,10 +6,11 @@ Import-Module Tools
 
 $deploymentName="AVD-Deployment-$(New-Guid)"
 
-$localVmAdminPassword = randomPassword
-$existingDomainAdminPassword = 'XgXqIlT6LYBydGnhvKd\cue/9q5k'#TODO: ONLY FOR TESTING PURPOSES
-
-#$existingDomainAdminPassword = 'S1stemcenteravd2021'
+#$localVmAdminPassword = randomPassword
+#$localVmAdminPassword = '7eVNqvRbCzmXwkUTK_vD8SKl5xsE'#rg-avd-prod-001 personal
+$localVmAdminPassword = 'x6zJgL4pcwF]w/4ccpjtcgaAtz]cij48e'#rg-avd-prod-001 pooled
+#$existingDomainAdminPassword = 'XgXqIlT6LYBydGnhvKd\cue/9q5k'#TODO: ONLY FOR TESTING PURPOSES
+$existingDomainAdminPassword = 'S1stemcenteravd2021' #virtualdom
 
 $params = "{ \`"localVmAdminPassword\`":{\`"value\`": \`"${localVmAdminPassword}\`" }, \`"existingDomainAdminPassword\`":{\`"value\`": \`"${existingDomainAdminPassword}\`" } }"
 
@@ -17,4 +18,4 @@ $params = "{ \`"localVmAdminPassword\`":{\`"value\`": \`"${localVmAdminPassword}
 # TODO: Ensure the parameters.json file is up to date
 # TODO: For production deployments, update the deployment parameter file in the command below.
 
-az deployment sub create -l westeurope -n $deploymentName --template-file '.\avd\main.bicep' --parameters '.\personal.parameters.json' --parameters $params
+az deployment sub create -l westeurope -n $deploymentName --template-file '.\avd\main.bicep' --parameters '.\pooled.parameters.virtualdom.json' --parameters $params
