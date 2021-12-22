f01e6d0a5ad4525142a6af83efff7a0e470b4c32
mlrcloud <manuellazaror@gmail.com>
2021-12-22 12:11:59 +0100

    clearing params variable

--- deploy.ps1
+++ deploy.ps1
@@ -1,6 +1,6 @@
 # Warning: Change parameters file based on desired deployment type: personal or pooled scenario
 # Warning: Running this script multiple times will cause the admin
-# password for the session host to be changed. Be sure to change the parameters file properly.
+# password for the session host to be changed. Be sure to change the parameters variables properly.
 
 Import-Module Tools
 
@@ -15,10 +15,10 @@ $localVmAdminPassword = 'x6zJgL4pcwF]w/4ccpjtcgaAtz]cij48e'
 
 #$localVmAdminPassword = '7eVNqvRbCzmXwkUTK_vD8SKl5xsE'#rg-avd-prod-001 personal
 #$localVmAdminPassword = 'x6zJgL4pcwF]w/4ccpjtcgaAtz]cij48e'#rg-avd-prod-001 pooled
-$existingDomainAdminPassword = 'XgXqIlT6LYBydGnhvKd\cue/9q5k'#TODO: ONLY FOR TESTING PURPOSES
+#$existingDomainAdminPassword = 'XgXqIlT6LYBydGnhvKd\cue/9q5k'#TODO: ONLY FOR TESTING PURPOSES
 #$existingDomainAdminPassword = 'S1stemcenteravd2021' #virtualdom
 
-$params = "{ \`"localVmAdminPassword\`":{\`"value\`": \`"${localVmAdminPassword}\`" }, \`"existingDomainAdminPassword\`":{\`"value\`": \`"${existingDomainAdminPassword}\`" } }"
+$params = "{ \`"localVmAdminPassword\`":{\`"value\`": \`"${localVmAdminPassword}\`" } }"
 
 # The deployment is applied at the subscription scope
 # Ensure the parameters.json file is up to date
