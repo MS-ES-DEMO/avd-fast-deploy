6b4f51915728724d7301a85e757046ddebe9ab5b
mlrcloud <manuellazaror@gmail.com>
2021-12-22 12:22:59 +0100

    updating steps

--- deploy.ps1
+++ deploy.ps1
@@ -2,15 +2,15 @@
 # Warning: Running this script multiple times will cause the admin
 # password for the session host to be changed. Be sure to change the parameters variables properly.
 
-Import-Module Tools
+Import-Module Tools # Comment if you prefer to set up the localVmAdminPassword parameter manually.
 
 $deploymentName="AVD-PRE-Data-Pooled-Deployment"
 #$deploymentName="AVD-PRE-Data-Pers-Deployment"
 #$deploymentName="AVD-PRE-Oper-Pooled-Deployment"
 #$deploymentName="AVD-PRE-Oper-Pers-Deployment"
 
-#$localVmAdminPassword = randomPassword
-$localVmAdminPassword = 'x6zJgL4pcwF]w/4ccpjtcgaAtz]cij48e' 
+$localVmAdminPassword = randomPassword # Comment wheter you prefer to set up this parameter manually or are running this script multiple times.
+
 
 
 #$localVmAdminPassword = '7eVNqvRbCzmXwkUTK_vD8SKl5xsE'#rg-avd-prod-001 personal
